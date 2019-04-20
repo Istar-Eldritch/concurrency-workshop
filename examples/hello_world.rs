3266dc4654e51a744f8142faaab8fab6f6cb00de
Ruben Paz <me@ruben.io>
2019-04-20 08:50:20 +0100

    Add log dependencies

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,9 +1,43 @@
+#![feature(test)]
+
+extern crate test;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 use std::thread;
 
-fn main() {
+fn hello_world() {
+    info!("Hello World");
+}
+
+fn hello_world_threaded() {
     let t = thread::spawn(|| {
-        println!("Hello, world!");
+        info!("Hello World Threaded!");
     });
 
     t.join().unwrap();
+}
+
+fn main() {
+    env_logger::init();
+    hello_world();
+    hello_world_threaded();
+}
+
+#[cfg(test)]
+mod test_hello {
+    use test::Bencher;
+    use super::{hello_world, hello_world_threaded};
+
+    #[bench]
+    fn bench_hello_world(b: &mut Bencher) {
+        b.iter(|| hello_world());
+    }
+
+    #[bench]
+    fn bench_hello_world_threaded(b: &mut Bencher) {
+        env_logger::init();
+        b.iter(|| hello_world_threaded());
+    }
 }
