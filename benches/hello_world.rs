70009fcd2fad7a3d196a1b4473544636566d5172
Ruben Paz <me@ruben.io>
2019-04-20 22:32:37 +0100

    Use criterion for benches

---
+++ benches/hello_world.rs
@@ -0,0 +1,19 @@
+#[macro_use]
+extern crate criterion;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use criterion::Criterion;
+
+pub fn hello_world() {
+    info!("Hello World");
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    env_logger::init();
+    c.bench_function("hello_world", |b| b.iter(|| hello_world()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
