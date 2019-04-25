bd6e0a454dd7426a7a9cfcb61e28f5e8d8579f50
mareq <mareq@balint.eu>
2019-04-25 19:10:50 +0100

    Correct valgrind command line for example #01

--- examples/01_create_thread.rs
+++ examples/01_create_thread.rs
@@ -2,7 +2,9 @@
 // 1. Create two threads with infinite loops, one of them using the builder API. Name your thread.
 // 2. Run your program and see if you can see it in your task manager. `eg. htop`
 // You can check the stack allocated with valgrind
-// valgrind --tool=massif  --massif-out-file=massif.out --stacks=yes ./target/debug/examples/02_thread_mem && grep mem_stacks_B massif.out | sed -e 's/mem_stacks_B=\(.*\)/\1/' | sort -g | tail -n 1
+// valgrind --tool=massif  --massif-out-file=massif.out --stacks=yes ./target/debug/examples/01_create_thread && grep mem_stacks_B massif.out | sed -e 's/mem_stacks_B=\(.*\)/\1/' | sort -g | tail -n 1
+use std::{thread, time};
+
 fn main() {
 
 }
